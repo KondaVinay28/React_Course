a52907f2023febe7018a185b8b7c9d85117dac2f
KondaVinay28 <kondavinay555@gmail.com>
2025-10-08 18:38:12 +0530

    Implemented Props Concept... added extra features to the Product Component without duplicating code

--- basic_react_app/src/App.jsx
+++ basic_react_app/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Title from "./Title";
 import ProductTab from "./ProductTab";
 
 function App() {
   return (
     <>
-      {/* <Title /> */} //both are same
+      <Title />
       <ProductTab />
     </>
   );
