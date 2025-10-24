327069310c6ae1093e4090cd02c0c3b4fc742fbd
KondaVinay28 <kondavinay555@gmail.com>
2025-10-24 17:46:24 +0530

    Using state or useState() hook to update a UI element dynamically

--- basic_react_app/src/Counter.jsx
+++ basic_react_app/src/Counter.jsx
@@ -1,14 +1,22 @@
-let count = 0;
-function handleClick() {
-  count++;
-  console.log(count);
-}
+import { useState } from "react";
+
+// let count = 0;
+// function handleClick() {
+//   count++;
+//   console.log(count);
+// }
 
 function Counter() {
+  const [count, setCount] = useState(0);
+  /* hooks should be defined inside a functional component */
+
+  function handleClick() {
+    setCount(count + 1);
+  }
   return (
     <>
       <h1>{count}</h1>
-      <button onClick={handleClick}>Counter</button>
+      <button onClick={handleClick}>Increment</button>
     </>
   );
 }
